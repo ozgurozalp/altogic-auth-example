cfd122b1ad521065e0403b186f6460acf3e757bb
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-04-27 00:10:00 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,45 @@
-import {Link, useNavigate} from "react-router-dom";
-import useAuth from "../hooks/useAuth";
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import Button from '../components/Button';
 
 export default function Home() {
 	const { user, logout, loading } = useAuth();
 	const navigate = useNavigate();
 
 	const clickHandler = async () => {
-	  const success = await logout();
-	  if (success) navigate("/login");
-	}
+		const success = await logout();
+		if (success) navigate('/login');
+	};
 
 	return (
 		<div className="h-full flex flex-col gap-4 justify-center items-center bg-gray-100">
 			<h1 className="text-5xl text-center">Welcome to Altogic Auth Sample</h1>
 			<div className="flex gap-2">
-				{
-					user
-						? <>
-							<Link to='/profile' className="px-4 py-2 rounded border bg-white transition-all hover:border-gray-500">Profile</Link>
-							<button onClick={clickHandler} className="px-4 py-2 rounded border bg-white transition-all hover:border-gray-500">
-								{loading ? 'Processing...' : 'Logout'}
-							</button>
-						</>
-						: <>
-							<Link to='/login' className="px-4 py-2 rounded border bg-white transition-all hover:border-gray-500">Login</Link>
-							<Link to='/register' className="px-4 py-2 rounded border bg-white transition-all hover:border-gray-500">Register</Link>
-						</>
-				}
-
+				{user ? (
+					<>
+						<Button renderAs="a" to="/profile">
+							Profile
+						</Button>
+						<Button onClick={clickHandler}>
+							{loading ? 'Processing...' : 'Logout'}
+						</Button>
+					</>
+				) : (
+					<>
+						<Link
+							to="/login"
+							className="px-4 py-2 rounded border bg-white transition-all hover:border-gray-500"
+						>
+							Login
+						</Link>
+						<Link
+							to="/register"
+							className="px-4 py-2 rounded border bg-white transition-all hover:border-gray-500"
+						>
+							Register
+						</Link>
+					</>
+				)}
 			</div>
 		</div>
 	);
