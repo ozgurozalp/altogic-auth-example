980089a3d9a74c96a38c7e4b46db2c08842e0904
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-04-27 00:09:52 +0300

    Create Button.tsx

---
+++ src/components/Button.tsx
@@ -0,0 +1,40 @@
+import type { ComponentPropsWithoutRef } from 'react';
+import { Link, LinkProps, To } from 'react-router-dom';
+
+export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
+	renderAs?: 'button' | 'a';
+	to?: To;
+	className?: string;
+}
+
+export default function Button({
+	children,
+	renderAs = 'button',
+	className,
+	to,
+	...props
+}: ButtonProps) {
+	if (renderAs === 'a') {
+		const linkProps = { to, ...props } as LinkProps;
+		return (
+			<Link
+				className={`px-4 py-2 rounded border bg-white transition-all hover:border-gray-500 ${
+					className ?? ''
+				}`}
+				{...linkProps}
+			>
+				{children}
+			</Link>
+		);
+	}
+	return (
+		<button
+			className={`px-4 py-2 rounded border bg-white transition-all hover:border-gray-500 ${
+				className ?? ''
+			}`}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+}
