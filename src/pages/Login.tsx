7eb5cc52ca451e413064a390e6349b0631118aa4
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-04-27 15:47:21 +0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import useAuth from '../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useId, useRef } from 'react';
 import type { FormEvent } from 'react';
+import Input from '../components/Input';
 
 export default function Login() {
 	const email = useRef<HTMLInputElement>(null);
@@ -43,33 +44,20 @@ export default function Login() {
 						))}
 					</div>
 				)}
-
 				<div className="input-group">
 					<label className="block text-sm mb-1 text-gray-700" htmlFor={emailID}>
 						Email
 					</label>
-					<input
-						className="rounded border-gray-400 outline-pink-600 focus:ring-2 focus:ring-gray-700 focus:border-gray-700"
-						id={emailID}
-						ref={email}
-						type="email"
-						required={true}
-					/>
+					<Input id={emailID} type="email" required={true} ref={email} />
 				</div>
 				<div className="input-group">
 					<label className="block text-sm mb-1 text-gray-700" htmlFor={passwordID}>
 						Password
 					</label>
-					<input
-						className="rounded border-gray-400 outline-pink-600 focus:ring-2 focus:ring-gray-700 focus:border-gray-700"
-						id={passwordID}
-						ref={password}
-						type="password"
-						required={true}
-					/>
+					<Input id={passwordID} type="password" required={true} ref={password} />
 				</div>
 				<button
-					className="px-4 py-2 border rounded border-gray-400 outline-pink-600 transition-all hover:border-gray-700 hover:ring-2 hover:ring-gray-700 text-gray-700 active:translate-y-1"
+					className="px-4 py-2 border rounded border-gray-400 transition-all hover:border-gray-700 hover:ring-2 hover:ring-gray-700 text-gray-700 active:translate-y-1"
 					type="submit"
 					disabled={loading}
 				>
