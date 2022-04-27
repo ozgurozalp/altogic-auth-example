65e6241c5d932f95d42a163f76156b3166055943
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-04-27 15:47:18 +0300

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -64,7 +64,7 @@ export default function Register() {
 						Email
 					</label>
 					<input
-						className="rounded border-gray-400 outline-pink-600 focus:ring-2 focus:ring-gray-700 focus:border-gray-700"
+						className="rounded border-gray-400 focus:ring-2 focus:ring-gray-700 focus:border-gray-700"
 						id={emailID}
 						ref={email}
 						type="email"
@@ -76,7 +76,7 @@ export default function Register() {
 						Password
 					</label>
 					<input
-						className="rounded border-gray-400 outline-pink-600 focus:ring-2 focus:ring-gray-700 focus:border-gray-700"
+						className="rounded border-gray-400 focus:ring-2 focus:ring-gray-700 focus:border-gray-700"
 						id={passwordID}
 						ref={password}
 						type="password"
@@ -88,7 +88,7 @@ export default function Register() {
 						Password Confirmation
 					</label>
 					<input
-						className="rounded border-gray-400 outline-pink-600 focus:ring-2 focus:ring-gray-700 focus:border-gray-700"
+						className="rounded border-gray-400 focus:ring-2 focus:ring-gray-700 focus:border-gray-700"
 						id={passwordConfID}
 						ref={passwordConf}
 						type="password"
@@ -96,7 +96,7 @@ export default function Register() {
 					/>
 				</div>
 				<button
-					className="px-4 py-2 border rounded border-gray-400 outline-pink-600 transition-all hover:border-gray-700 hover:ring-2 hover:ring-gray-700 text-gray-700 active:translate-y-1"
+					className="px-4 py-2 border rounded border-gray-400 transition-all hover:border-gray-700 hover:ring-2 hover:ring-gray-700 text-gray-700 active:translate-y-1"
 					type="submit"
 					disabled={loading}
 				>
