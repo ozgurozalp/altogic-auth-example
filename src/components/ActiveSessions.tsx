f77524ed58f7829061fdeda1882f28bf8c9ca566
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-04-30 00:28:08 +0300

    Create ActiveSessions.tsx

---
+++ src/components/ActiveSessions.tsx
@@ -0,0 +1,62 @@
+import { format } from 'date-fns';
+import useAuth from '../hooks/useAuth';
+import Button from './Button';
+import { useEffect, useState } from 'react';
+import type { Session } from 'altogic';
+
+interface ActiveSessionsProps {
+	className?: string;
+}
+
+export default function ActiveSessions({ className }: ActiveSessionsProps) {
+	const [sessions, setSessions] = useState<Session[] | null>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string>('');
+	const { getAllSessions, logout } = useAuth();
+
+	useEffect(() => {
+		setLoading(true);
+		getAllSessions().then(res => {
+			if (res.errors) return setError("Can't get sessions");
+			if (res.success) setSessions(res.sessions);
+			setLoading(false);
+		});
+	}, []);
+
+	const clickHandle = async (token: string | undefined) => {
+		console.log(token);
+		const { success } = await logout(token);
+		console.log(success);
+	};
+
+	if (!sessions) return null;
+	return (
+		<div className={`bg-white grid gap-4 ${className ?? ''}`}>
+			{loading ? (
+				<h2 className="text-center text-xl font-medium py-2">Loading...</h2>
+			) : (
+				<>
+					<h2 className="text-center text-xl font-medium py-2">All Sessions</h2>
+					{sessions.map(session => (
+						<div
+							key={session.token}
+							className="flex items-center justify-between border p-4"
+						>
+							<div>
+								<div>
+									Device: <strong>{session.userAgent.family}</strong>
+								</div>
+								<time className="text-gray-600 text-xs">
+									{format(new Date(session.creationDtm), 'yyyy-MM-dd HH:mm:ss')}
+								</time>
+							</div>
+							<Button onClick={() => clickHandle(session.token)}>
+								Logout from this
+							</Button>
+						</div>
+					))}
+				</>
+			)}
+		</div>
+	);
+}
