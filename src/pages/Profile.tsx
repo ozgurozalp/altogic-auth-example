7f96c72b2f95f5290c0ce2e5639ea517e96266d2
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-04-29 15:00:51 +0300

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -7,12 +7,12 @@ export default function Profile() {
 	const navigate = useNavigate();
 
 	const clickHandler = async () => {
-		const success = await logout();
+		const { success } = await logout();
 		if (success) navigate('/login');
 	};
 
 	return (
-		<section className="h-full flex items-center justify-center flex-col gap-4 bg-gray-100">
+		<section className="h-full flex items-center justify-center flex-col gap-4">
 			<h2 className="text-6xl text-center">
 				Welcome <strong className="block">{user?.email} 👋</strong>
 			</h2>
